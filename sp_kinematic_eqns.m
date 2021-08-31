fbe7f7f485fa7761cbebc01395069d7a003b57e0
Samiran Datta <56483357+SamiranDatta@users.noreply.github.com>
2021-08-31 11:52:40 +0530

    Update sp_kinematic_eqns.m

--- sp_kinematic_eqns.m
+++ sp_kinematic_eqns.m
@@ -4,57 +4,78 @@ close all
 
 syms phi(t) theta(t) psi(t) X Y Z xa ya za xb yb zb
 
-xa = 0.225
-ya = 0.0
-za = -0.228
+phi = 0;
+theta = 0;
+psi = 0;
+X = 0;
+Y = 0;
+Z = 0;
+xa = 0;
+ya = 0;
+za = 0;
+xb = 0;
+yb = 0;
+zb = 0;
 
-xb = 1.7580
-yb = 2.8
-zb = -1.015
+fprintf('P is the platform coordinate system and W is the world coordinate system')
 
-X = 0.2
-Y = 0.3
-Z = -0.4
+for i=1:6
+    fprintf('\nEnter values of link %d:- ', i)
+    xa = input('\nEnter X coordinate of attachment point of platform with reference to P: ');
+    ya = input('\nEnter Y coordinate of attachment point of platform with reference to P: ');
+    za = input('\nEnter Z coordinate of attachment point of platform with reference to P: ');
 
-phi = 0.1
-theta = -1.4
-psi = 0.1
+    xb = input('\nEnter X coordinate of attachment point of base with reference to W: ');
+    yb = input('\nEnter Y coordinate of attachment point of base with reference to W: ');
+    zb = input('\nEnter Z coordinate of attachment point of base with reference to W: ');
 
-phidot = diff(phi, t);
-thetadot = diff(theta, t);
-psidot = diff(psi, t);
-phidotdot = diff(phidot, t);
-thetadotdot = diff(thetadot, t);
-psidotdot = diff(psidot, t);
+    X = input('\nEnter X coordinate of frame P with reference to base coordinate system W: ');
+    Y = input('\nEnter Y coordinate of frame P with reference to base coordinate system W: ');
+    Z = input('\nEnter Z coordinate of frame P with reference to base coordinate system W: ');
 
-%rotation matrix
-R = [cos(psi)*cos(phi)-cos(theta)*sin(phi)*sin(psi) -sin(psi)*cos(phi)-cos(theta)*sin(phi)*cos(psi) sin(theta)*sin(phi); cos(psi)*sin(phi)+cos(theta)*cos(phi)*sin(psi) -sin(psi)*sin(phi)+cos(theta)*cos(phi)*cos(psi) -sin(theta)*cos(phi); sin(psi)*sin(theta) cos(psi)*sin(theta) cos(theta)];
+    phi = input('\nEnter roll angle: ');
+    theta = input('\nEnter pitch angle: ');
+    psi = input('\nEnter yaw angle: ');
 
-%angular velocity
-omegaX = [0 cos(phi) sin(phi)*cos(theta)]*[phidot; thetadot; psidot];
-omegaY = [0 sin(phi) -cos(phi)*sin(theta)]*[phidot; thetadot; psidot];
-omegaZ = [1 0 cos(theta)]*[phidot; thetadot; psidot];
-omega = [omegaX; omegaY; omegaZ]
+    phidot = diff(phi, t);
+    thetadot = diff(theta, t);
+    psidot = diff(psi, t);
+    phidotdot = diff(phidot, t);
+    thetadotdot = diff(thetadot, t);
+    psidotdot = diff(psidot, t);
 
-%angular acceleration
-alphaX = [0 cos(phi) sin(phi)*cos(theta)]*[phidotdot; thetadotdot; psidotdot] + [0 -phidot*sin(phi) phidot*cos(phi)*sin(theta)+thetadot*sin(phi)*cos(theta)]*[phidot; thetadot; psidot];
-alphaY = [0 sin(phi) -cos(phi)*sin(theta)]*[phidotdot; thetadotdot; psidotdot] + [0 phidot*cos(phi) phidot*sin(phi)*sin(theta)-thetadot*cos(phi)*cos(theta)]*[phidot; thetadot; psidot];
-alphaZ = [1 0 cos(theta)]*[phidotdot; thetadotdot; psidotdot] + [0 0 -thetadot*sin(theta)]*[phidot; thetadot; psidot];
-alpha = [alphaX; alphaY; alphaZ];
+    %rotation matrix
+    R = [cos(psi)*cos(phi)-cos(theta)*sin(phi)*sin(psi) -sin(psi)*cos(phi)-cos(theta)*sin(phi)*cos(psi) sin(theta)*sin(phi); 
+        cos(psi)*sin(phi)+cos(theta)*cos(phi)*sin(psi) -sin(psi)*sin(phi)+cos(theta)*cos(phi)*cos(psi) -sin(theta)*cos(phi); 
+        sin(psi)*sin(theta) cos(psi)*sin(theta) cos(theta)];
 
-%translation vector
-x = [X; Y; Z];
+    %angular velocity
+    omegaX = [0 cos(phi) sin(phi)*cos(theta)]*[phidot; thetadot; psidot];
+    omegaY = [0 sin(phi) -cos(phi)*sin(theta)]*[phidot; thetadot; psidot];
+    omegaZ = [1 0 cos(theta)]*[phidot; thetadot; psidot];
+    omega = [omegaX; omegaY; omegaZ]
 
-%coordinates of attachment point on moving platform
-Pa = [xa; ya; za];
+    %angular acceleration
+    alphaX = [0 cos(phi) sin(phi)*cos(theta)]*[phidotdot; thetadotdot; psidotdot] + [0 -phidot*sin(phi) phidot*cos(phi)*sin(theta)+thetadot*sin(phi)*cos(theta)]*[phidot; thetadot; psidot];
+    alphaY = [0 sin(phi) -cos(phi)*sin(theta)]*[phidotdot; thetadotdot; psidotdot] + [0 phidot*cos(phi) phidot*sin(phi)*sin(theta)-thetadot*cos(phi)*cos(theta)]*[phidot; thetadot; psidot];
+    alphaZ = [1 0 cos(theta)]*[phidotdot; thetadotdot; psidotdot] + [0 0 -thetadot*sin(theta)]*[phidot; thetadot; psidot];
+    alpha = [alphaX; alphaY; alphaZ];
 
-%transformation mtarix
-a = x + R*Pa;
+    %translation vector
+    x = [X; Y; Z];
 
-%coordinates of attachment point on base
-b = [xb; yb; zb];
+    %coordinates of attachment point on moving platform
+    Pa = [xa; ya; za];
 
-L = a - b
+    %transformation matrix
+    a = x + R*Pa;
 
-%length of link
-l = sqrt(dot(L, L))
+    %coordinates of attachment point on base
+    b = [xb; yb; zb];
+
+    L = a - b
+
+    %length of link
+    l = sqrt(dot(L, L))
+    fprintf('\nLength of link %d is: %f', i, l)
+end
